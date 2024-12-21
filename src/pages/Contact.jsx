20a1520a2d645f016657a6f5de106001bd7427d9
rustammgasanovv <rustammgasanovv@gmail.com>
2024-12-21 18:11:33 +0400

    first commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Feel free to contact us.</p>
+    </div>
+  );
+}
+
+export default Contact;
